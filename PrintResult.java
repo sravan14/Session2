4becf91fa9efdc1cfa1c14ad81f0676755a55634
sravan14 <sravanforeveryone@gmail.com>
2016-12-18 01:43:04 +0530

    Add files via upload

---
+++ PrintResult.java
@@ -0,0 +1,19 @@
+
+public class PrintResult {
+	
+	public static void main(String[] args){
+		int a =20;
+		int b=10;
+		 b=a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		int e=a&b;
+		
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+
+}
